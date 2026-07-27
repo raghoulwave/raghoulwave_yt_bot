3b47129e31c3e53ac59f04a26dab6a3f956d1c23
John Minecraft <x83x.straightedge.x83x@gmail.com>
2026-07-27 18:26:39 +0200

    Initial commit

---
+++ src/main/java/ua/raghoulwave/raghoulwave_yt_bot/RaghoulwaveYtBotApplication.java
@@ -0,0 +1,13 @@
+package ua.raghoulwave.raghoulwave_yt_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaghoulwaveYtBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RaghoulwaveYtBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ua/raghoulwave/raghoulwave_yt_bot/RaghoulwaveYtBotApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.raghoulwave.raghoulwave_yt_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaghoulwaveYtBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
