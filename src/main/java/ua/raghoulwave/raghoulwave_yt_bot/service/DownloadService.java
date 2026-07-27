a5d95cafebeaef63688b0fb78e5c5a5a1d5904db
John Minecraft <x83x.straightedge.x83x@gmail.com>
2026-07-27 21:56:57 +0200

    feat: DownloadService

---
+++ src/main/java/ua/raghoulwave/raghoulwave_yt_bot/service/DownloadService.java
@@ -0,0 +1,20 @@
+package ua.raghoulwave.raghoulwave_yt_bot.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import ua.raghoulwave.raghoulwave_yt_bot.properties.YtDlpProperties;
+
+import java.nio.file.Path;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class DownloadService {
+
+    private final YtDlpProperties properties;
+
+    public Path downloadDirectory() {
+        return properties.downloadDir();
+    }
+}
